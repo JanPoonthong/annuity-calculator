85fcacec89770537d3406d5aebbe5f5d281d531b
Jan Poonthong <janpoonthong628@gmail.com>
2024-03-09 08:00:45 +0700

    style: format code with black

--- a.py
+++ a.py
@@ -1,6 +1,7 @@
 def calculate_annuity(interest_rate, periods):
-    annuity = (1 - (1 + interest_rate)**-periods) / interest_rate
+    annuity = (1 - (1 + interest_rate) ** -periods) / interest_rate
     return round(annuity, 6)
+
 
 interest_rate = float(input("Enter the interest rate per period: "))
 periods = int(input("Enter the number of periods: "))
--- s.py
+++ s.py
@@ -1,6 +1,7 @@
 def calculate_s(interest_rate, periods):
-    annuity = (((1 + interest_rate)**periods) - 1) / interest_rate
+    annuity = (((1 + interest_rate) ** periods) - 1) / interest_rate
     return round(annuity, 6)
+
 
 interest_rate = float(input("Enter the interest rate per period: "))
 periods = int(input("Enter the number of periods: "))
