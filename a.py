0e647234f12a694d9c2b011a7ad80c90967c047f
Jan Poonthong <janpoonthong628@gmail.com>
2024-03-09 08:00:18 +0700

    feat: program to calculate annuity(a and s)

---
+++ a.py
@@ -0,0 +1,9 @@
+def calculate_annuity(interest_rate, periods):
+    annuity = (1 - (1 + interest_rate)**-periods) / interest_rate
+    return round(annuity, 6)
+
+interest_rate = float(input("Enter the interest rate per period: "))
+periods = int(input("Enter the number of periods: "))
+
+annuity_amount = calculate_annuity(interest_rate, periods)
+print(f"The annuity amount is: {annuity_amount}")
---
+++ s.py
@@ -0,0 +1,9 @@
+def calculate_s(interest_rate, periods):
+    annuity = (((1 + interest_rate)**periods) - 1) / interest_rate
+    return round(annuity, 6)
+
+interest_rate = float(input("Enter the interest rate per period: "))
+periods = int(input("Enter the number of periods: "))
+
+s_amount = calculate_s(interest_rate, periods)
+print(f"The s amount is: {s_amount}")
